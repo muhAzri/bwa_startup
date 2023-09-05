984414fbf43ec69757a4cc821401cca820193028
Muhammad Azri Fatihah Susanto <muhammad.azri.f.s@gmail.com>
2023-09-05 10:13:13 +0700

    refactor(handler):better-message

--- handler/campaign.go
+++ handler/campaign.go
@@ -17,8 +17,7 @@ func NewCampaignHandler(service campaign.Service) *campaignHandler {
 	return &campaignHandler{service: service}
 }
 
-//api/v1/campaign
-
+// API endpoint: GET /api/v1/campaigns
 func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	userIDStr := c.Query("user_id")
 	pageStr := c.Query("page")
@@ -54,34 +53,35 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	campaigns, err := h.service.GetCampaign(userID, page, limit)
 
 	if err != nil {
-		response := helper.ApiResponse("Error fetching campaigns", http.StatusInternalServerError, "error", nil, err.Error())
+		response := helper.ApiResponse("Failed to fetch campaigns", http.StatusInternalServerError, "error", nil, err.Error())
 		c.JSON(http.StatusInternalServerError, response)
 		return
 	}
 
-	response := helper.ApiResponse("List of campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns), nil)
+	response := helper.ApiResponse("Campaigns retrieved successfully", http.StatusOK, "success", campaign.FormatCampaigns(campaigns), nil)
 	c.JSON(http.StatusOK, response)
 
 }
 
+// API endpoint: GET /api/v1/campaign/:id
 func (h *campaignHandler) GetCampaign(c *gin.Context) {
 	var input campaign.GetCampaignDetailInput
 
 	err := c.ShouldBindUri(&input)
 
 	if err != nil {
-		response := helper.ApiResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil, err.Error())
+		response := helper.ApiResponse("Failed to get campaign details: Invalid request", http.StatusBadRequest, "error", nil, err.Error())
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
 	campaignDetail, err := h.service.GetCampaignByID(input)
 	if err != nil {
-		response := helper.ApiResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil, err.Error())
+		response := helper.ApiResponse("Failed to get campaign details: Campaign not found", http.StatusBadRequest, "error", nil, err.Error())
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.ApiResponse("Campaign detail", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail), nil)
+	response := helper.ApiResponse("Campaign details retrieved successfully", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail), nil)
 	c.JSON(http.StatusOK, response)
 }
